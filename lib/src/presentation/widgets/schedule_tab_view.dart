509a427664286409739411df68fdd5f19694ef04
bongcoy <mpascaldewantara@gmail.com>
2023-06-11 17:33:25 +0700

    update: workout_program_detail_screen.dart

--- lib/src/presentation/widgets/schedule_tab_view.dart
+++ lib/src/presentation/widgets/schedule_tab_view.dart
@@ -2,50 +2,56 @@ import 'package:flutter/material.dart';
 import 'package:specifit/src/presentation/widgets/buttons/day_button.dart';
 import 'package:specifit/src/presentation/widgets/cards/workout_program_detail_item.dart';
 
-import '../themes.dart';
-
 class ScheduleTabView extends StatelessWidget {
   const ScheduleTabView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 56,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 7,
-                itemBuilder: (ctx, idx) {
-                  return DayButton(
-                    nDay: (idx + 1).toString(),
-                  );
-                },
-              ),
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(16),
+            color: Colors.white,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 56,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 7,
+                    itemBuilder: (ctx, idx) {
+                      return DayButton(
+                        nDay: (idx + 1).toString(),
+                      );
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const Text("Olahraga"),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.22,
+                  child: ListView.builder(
+                    itemCount: 3,
+                    itemBuilder: (ctx, idx) {
+                      return const WorkoutProgramDetailItem();
+                    },
+                  ),
+                ),
+              ],
             ),
-            const Text("Olahraga"),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.22,
-              child: ListView.builder(
-                itemCount: 3,
-                itemBuilder: (ctx, idx) {
-                  return const WorkoutProgramDetailItem();
-                },
-              ),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            Container(
-              height: MediaQuery.of(context).size.height * 0.16,
-              color: orangeColor,
-            ),
-          ],
-        ),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Center(
+            child: Image.asset("assets/images/banner_meal_plan.png"),
+          ),
+        ],
       ),
     );
   }
